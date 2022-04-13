6b19a6c04dd841b15c268afc6d0ca8e52cb73e24
Kovalski <81390604+Kovalski-rgb@users.noreply.github.com>
2022-04-13 10:44:11 -0300

    Update temperatura.pl

--- temperatura.pl
+++ temperatura.pl
@@ -1,3 +1,12 @@
+/*
+write("(1) Fahrenheit para Celcius"),nl,
+write("(2) Celcius para Fahrenheit"),
+read(X),
+write("Temperatura: "),
+read(Y),
+temperatura(X, Y, Z)
+*/
+
 temperatura(X, Y, Z):- (X == 1 -> fparaC(Y, Z); cparaF(Y, Z)).
 fparaC(Y, Z):- Z is (Y-32)*5/9, write(Z).
 cparaF(Y, Z):- Z is (Y*(9/5))+32, write(Z).
